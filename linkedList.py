9ace678ea274cd1d4ed4206b52ac2c5efc52b8fb
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-01-01 19:44:26 +1300

    Basic implementation of linked-list data structure
    Coded without any external reference.

---
+++ linkedList.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class linkedList:
+    def __init__(self, head):
+        self.head = None
+    
+    def insert(self, newNode):
+        if self.head is None:
+            self.head = newNode
+        else:
+            lastNode = self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode = lastNode.next
+            lastNode = newNode
+
+
+    def printList(self):
+        currentNode = self.head
+        while True:
+            if currentNode.next is None:
+                break
+            print(currentNode)
+            currentNode = currentNode.next
+
+
+firstGuy = Node("Bob")
+fancyList = linkedList(firstGuy)
+fancyList.insert(firstGuy)
+
+secondGuy = Node("Caleb")
+fancyList = linkedList(secondGuy)
+fancyList.insert(secondGuy)
+
+thirdGuy = Node("Dave")
+fancyList = linkedList(thirdGuy)
+fancyList.insert(thirdGuy)
+
+fancyList.printList()
+
+              
